a13ae04b4786a42d0a4119fc9ba6eefc251ea87a
bandhandut19 <bondon21081@gmail.com>
2023-10-22 13:56:06 +0600

    fixed insertion issues with userInfo

--- index.js
+++ index.js
@@ -23,109 +23,107 @@ const client = new MongoClient(uri, {
   }
 });
 
-const tech_bond = client.db('tech_bond')
-const allProducts = tech_bond.collection('allProducts')
-const userCart = tech_bond.collection('userCart')
-const userInfo = tech_bond.collection('userInfo')
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const tech_bond = client.db('tech_bond')
+    const allProducts = tech_bond.collection('allProducts')
+    const userCart = tech_bond.collection('userCart')
+    const userInfo = tech_bond.collection('userInfo')
 
-    app.get('/userInfo',async(req,res)=>{
+    app.get('/userinfo', async (req, res) => {
       const cursor = userInfo.find()
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.post('/userInfo',async(req,res)=>{
+    app.post('/userinfo', async (req, res) => {
       const info = req.body
       const result = await userInfo.insertOne(info)
       res.send(result)
+      console.log(result)
     })
 
 
-    app.get('/usercart',async(req,res)=>{
+    app.get('/usercart', async (req, res) => {
       const cursor = userCart.find()
       const result = await cursor.toArray()
       res.send(result)
-  })
+    })
 
 
-  app.post('/usercart',async (req,res)=>{
+    app.post('/usercart', async (req, res) => {
       const productInfo = req.body
-      const result  = await userCart.insertOne(productInfo)
+      const result = await userCart.insertOne(productInfo)
       res.send(result)
       console.log(result)
-  })
+    })
 
 
 
-  // app.delete('/usercart/:id',async(req,res)=>{
-  //   const id = req.params.id
-  //   const query = {_id: new ObjectId(id)}
-  //   const result = await userCart.deleteOne(query)
-  //   res.send(result)
-  //   console.log(result)
-  // })
+    // app.delete('/usercart/:id',async(req,res)=>{
+    //   const id = req.params.id
+    //   const query = {_id: new ObjectId(id)}
+    //   const result = await userCart.deleteOne(query)
+    //   res.send(result)
+    //   console.log(result)
+    // })
 
-  // Updated API route with error handling
-app.delete('/usercart/:id', async (req, res) => {
-  try {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCart.deleteOne(query);
-      
-      if (result.deletedCount > 0) {
+    // Updated API route with error handling
+    app.delete('/usercart/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await userCart.deleteOne(query);
+
+        if (result.deletedCount > 0) {
           console.log("Successfully deleted");
           res.status(200).json({ message: "Item successfully deleted" });
-      } else {
+        } else {
           console.log("Item not found");
           res.status(404).json({ message: "Item not found" });
+        }
+      } catch (error) {
+        console.error("Error deleting item:", error);
+        res.status(500).json({ message: "Internal server error" });
       }
-  } catch (error) {
-      console.error("Error deleting item:", error);
-      res.status(500).json({ message: "Internal server error" });
-  }
-});
+    });
 
-
-
-
-    app.get('/allproducts',async(req,res)=>{
-        const cursor = allProducts.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get('/allproducts', async (req, res) => {
+      const cursor = allProducts.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
 
 
-    app.post('/allproducts',async (req,res)=>{
-        const productInfo = req.body
-        const result  = await allProducts.insertOne(productInfo)
-        res.send(result)
-        console.log(result)
+    app.post('/allproducts', async (req, res) => {
+      const productInfo = req.body
+      const result = await allProducts.insertOne(productInfo)
+      res.send(result)
+      console.log(result)
     })
 
-    app.put('/allproducts/:id',async (req,res)=>{
+    app.put('/allproducts/:id', async (req, res) => {
       const id = req.params.id
-      const filter = {_id: new ObjectId(id)}
-      const options = {upset: true}
+      const filter = { _id: new ObjectId(id) }
+      const options = { upset: true }
       const updatedProductInfo = req.body
       const productInfo = {
         $set: {
-          image:updatedProductInfo.image,
-          name:updatedProductInfo.name,
-          brandName:updatedProductInfo.brandName,
-          type:updatedProductInfo.type,
-          price:updatedProductInfo.price,
-          description:updatedProductInfo.description,
-          rating:updatedProductInfo.rating
+          image: updatedProductInfo.image,
+          name: updatedProductInfo.name,
+          brandName: updatedProductInfo.brandName,
+          type: updatedProductInfo.type,
+          price: updatedProductInfo.price,
+          description: updatedProductInfo.description,
+          rating: updatedProductInfo.rating
 
         }
       }
-      const result = await allProducts.updateOne(filter,productInfo,options)
+      const result = await allProducts.updateOne(filter, productInfo, options)
       res.send(result)
 
 
@@ -157,11 +155,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send("this is server main page")
+app.get('/', (req, res) => {
+  res.send("this is server main page")
 })
 
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on ${port}`)
 })
 

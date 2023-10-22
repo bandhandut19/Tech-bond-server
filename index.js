a235ba26a918b7e188c5352a0745828d8de49b95
bandhandut19 <bondon21081@gmail.com>
2023-10-22 22:54:28 +0600

    server dploy issue solved

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const tech_bond = client.db('tech_bond')
     const allProducts = tech_bond.collection('allProducts')
@@ -133,7 +133,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
